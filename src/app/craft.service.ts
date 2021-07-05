8742bd6722ba227ca4cc62d345ec8ca0cafcc243
Shashini Maheshika Gunawardhana <smgunawardhana96@gmail.com>
2021-07-04 18:56:34 -0700

    Get item list method was created

--- src/app/craft.service.ts
+++ src/app/craft.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CraftService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  // Service method to get all item list from the web api
+  getItemList(): Observable<string[]> {
+    return this.http.get<string[]>('https://localhost:5001/api/Item');
+  }
 }
