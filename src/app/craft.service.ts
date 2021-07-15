562f2f84d7b06eee2111cbe1ca09bf37d9fa7dae
Shashini Maheshika Gunawardhana <smgunawardhana96@gmail.com>
2021-07-15 08:18:46 -0700

    Tab index transfer

--- src/app/craft.service.ts
+++ src/app/craft.service.ts
@@ -1,16 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CraftService {
+  // For tab index
+  private tabSource = new BehaviorSubject(0);
+  currentTab = this.tabSource.asObservable();
+
   constructor(private http: HttpClient) {}
+
+  // Change tab index
+  changeTabIndex(index: number) {
+    this.tabSource.next(index);
+  }
 
   // Service method to get all item list from the web api
   getItemList(): Observable<string[]> {
     return this.http.get<string[]>('https://localhost:5001/api/Item');
+  }
+
+  // Method to get all products details from the web api
+  getProductFromWebApi(): Observable<string[]> {
+    return this.http.get<string[]>('https://localhost:5001/api/Product');
   }
 }
