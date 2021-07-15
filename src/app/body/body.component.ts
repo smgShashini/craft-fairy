85e93d976395d1124ebc1d241f149b030ba99ca4
Shashini Maheshika Gunawardhana <smgunawardhana96@gmail.com>
2021-07-15 08:19:49 -0700

    Products in tabs are completed

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { CraftService } from '../craft.service';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.css']
+  styleUrls: ['./body.component.css'],
 })
 export class BodyComponent implements OnInit {
+  recievedProductList: any[] = []; // Store recieved product objects
+  displayList: any[] = []; // Store display list of products in each category
+  recievedTab: number | undefined; // Store recieved tab index
+  subscription: any;
+  msg: string = '';
 
-  constructor() { }
+  constructor(private craftService: CraftService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    // Get recieved tab index from category component
+    this.subscription = this.craftService.currentTab.subscribe(
+      (index) => (this.recievedTab = index)
+    );
+    if (this.recievedTab == 0) {
+      // Get product list from web api
+      this.craftService.getProductFromWebApi().subscribe((data) => {
+        this.recievedProductList = data;
+        // Create display list
+        for (var i = 0; i < this.recievedProductList.length; i++) {
+          if (this.recievedProductList[i].rate == 1) {
+            this.displayList.push(this.recievedProductList[i]);
+          }
+        }
+      });
+    } else {
+      // Get product list from web api
+      this.craftService.getProductFromWebApi().subscribe((data) => {
+        this.recievedProductList = data;
+        // Create display list
+        for (var i = 0; i < this.recievedProductList.length; i++) {
+          if (this.recievedProductList[i].categoryId == this.recievedTab) {
+            this.displayList.push(this.recievedProductList[i]);
+          }
+        }
+      });
+    }
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
+  // Create new list with recieved tab
+  createDisplayList() {
+    for (var i = 0; i < this.recievedProductList.length; i++) {
+      if (this.recievedProductList[i].categoryId == this.recievedTab) {
+        this.displayList.push(this.recievedProductList[i]);
+      }
+    }
+  }
 }
--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTabChangeEvent } from '@angular/material/tabs';
+import { CraftService } from '../craft.service';
 
 @Component({
   selector: 'app-category',
@@ -7,18 +8,80 @@ import { MatTabChangeEvent } from '@angular/material/tabs';
   styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
+  subscription: any;
+  active: boolean = true;
+  paperActive: boolean = false;
+  woodenActive: boolean = false;
+  glassActive: boolean = false;
+  ironActive: boolean = false;
+  clothActive: boolean = false;
+
   //displayCategory = true; // Display and hide category
   option: string = '';
   events: string[] = [];
   tab = 0; // Store the index of selected tab
-  constructor() {}
+  recicurrentTab: number | undefined;
 
-  ngOnInit(): void {}
+  constructor(private craftService: CraftService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.craftService.currentTab.subscribe(
+      (index) => (this.recicurrentTab = index)
+    );
+    console.log(this.recicurrentTab);
+
+    // Sending tab index
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
   selectChangeHandler(tabChangeEvent: MatTabChangeEvent) {
-    console.log('tabChangeEvent => ', tabChangeEvent);
-    console.log('index => ', tabChangeEvent.index);
-    this.tab = tabChangeEvent.index;
-    console.log(this.tab);
+    switch (tabChangeEvent.index) {
+      case 0: {
+        this.craftService.changeTabIndex(0);
+
+        break;
+      }
+
+      case 1: {
+        this.craftService.changeTabIndex(1);
+        this.paperActive = true;
+        break;
+      }
+
+      case 2: {
+        this.craftService.changeTabIndex(2);
+        this.woodenActive = true;
+        break;
+      }
+
+      case 3: {
+        this.craftService.changeTabIndex(3);
+        this.glassActive = true;
+        break;
+      }
+      case 4: {
+        this.craftService.changeTabIndex(4);
+        this.clothActive = true;
+        break;
+      }
+
+      case 5: {
+        this.craftService.changeTabIndex(5);
+        this.ironActive = true;
+        break;
+      }
+    }
+
+    // console.log('tabChangeEvent => ', tabChangeEvent);
+    // console.log('index => ', tabChangeEvent.index);
+    // this.tab = tabChangeEvent.index;
+    // console.log(typeof this.tab);
+
+    // //change index
+    // console.log('Sending index ' + this.tab);
 
     // tabChanged(tabChangeEvent: MatTabChangeEvent): void {
     //   console.log('tabChangeEvent => ', tabChangeEvent);
